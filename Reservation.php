59dad507fe212423fcaf16220a8df486132e8bd6
Steven <vanlaethem.steven@gmail.com>
2021-06-11 14:56:13 +0200

    class reservation ajout des requetes

--- Reservation.php
+++ Reservation.php
@@ -5,8 +5,8 @@ class Reservation {
     private $_id;
     private $_dated;
     private $_hour;
-    private $_user;
-    private $_resto;
+    private $_id_user;
+    private $_id_resto;
    
 
     
@@ -29,20 +29,69 @@ class Reservation {
     public function getHour(){
         return $this->_hour;
     }
-    public function setUser($user){
-        $this->_user= $user;
+    public function setId_user($id_user){
+        $this->_id_user= $id_user;
     }
 
-    public function getUser(){
-        return $this->_user;
+    public function getId_user(){
+        return $this->_id_user;
     }
-    public function setResto($resto){
-        $this->_resto= $resto;
-    }o
+    public function setId_resto($id_resto){
+        $this->_id_resto= $id_resto;
+    }
 
-    public function getResto(){
-        return $this->_resto;
+    public function getId_resto(){
+        return $this->_id_resto;
     }
     
 
     }
+
+
+    public function envoisDonnees($dsn,$user,$password){
+
+        try{
+            $dbh = new PDO($dsn,$user,$password);
+        }catch(PDOException $e){
+            echo 'cheh';
+        }
+        $sth= $dbh->prepare("INSERT INTO `Reservation`(`dated`, `hour`, `id_user`, `id_resto`) 
+        VALUES(:dated, :hour, :id_user, :id_resto,);");
+
+        $sth->bindParam(':dated', $this->getDated());
+        $sth->bindParam(':hour', $this->getHour());
+        $sth->bindParam(':id_user', $this->getId_user());
+        $sth->bindParam(':id_resto', $this->getId_resto());
+        
+
+        $sth->execute();
+    }
+
+    public function recupDonnees($dsn,$user,$password){
+
+        try{
+            $dbh= new PDO($dsn,$user,$password);
+    
+        $sth = $dbh->prepare("SELECT * FROM `Reservation`");
+        
+        $sth->execute();
+        $count = $sth->rowCount();
+         
+        $result = $sth->fetchAll();
+    
+        echo var_dump($result);
+        }catch(PDOException $e){
+            $e->getMessage();
+        }
+    
+        if($count > 0   && !empty($result)){
+    
+            return $result;
+            
+    
+        }else{
+            header("Location: connexion.php");
+        }
+
+    }
+}
